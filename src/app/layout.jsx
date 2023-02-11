7a067234d509921f4bfc6c0cc556c107f62ed2c7
James Heangboone <james623915@gmail.com>
2023-02-11 16:25:01 -0600

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,18 @@
 import Header from "@/components/Header";
 import "./globals.css";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
-        {/* Searchbox  */}
-        {children}
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
+          {/* Searchbox  */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
