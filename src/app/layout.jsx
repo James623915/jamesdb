263f708e1848cb7b6f4164f1cca7e41459d1ee42
James Heangboone <james623915@gmail.com>
2023-02-11 13:54:22 -0600

    create the header section

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Header */}
+        <Header />
+        {/* Navbar */}
+        {/* Searchbox  */}
+        {children}
+      </body>
+    </html>
+  );
+}
