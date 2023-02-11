5ca29059332a126d850da3daaa5552e78763d6b6
James Heangboone <james623915@gmail.com>
2023-02-11 08:55:15 -0600

    install next js

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>jamesdb</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This jamesdb website." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
